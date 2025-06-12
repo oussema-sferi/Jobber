434c79f471e84b936a788b1777a9dab4567adcc5
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-13 00:39:13 +0100

    feat(header): integrate header template & conditional css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     @vite('resources/css/app.css')
     <title>{{$title ?? 'Jobber | Find and list jobs'}}</title>
 </head>
 <body>
-<x-header />
+<x-header/>
 <main class="container mx-auto p-4 mt-4 bg-amber-100">
     {{$slot}}
 </main>
