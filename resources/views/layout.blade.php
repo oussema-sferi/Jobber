c6cff403c6238473f916d3db1f467e6b81f4ef33
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-13 15:33:13 +0100

    feat(hero): add & integrate Hero component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,14 +8,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
           integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>{{$title ?? 'Jobber | Find and list jobs'}}</title>
 </head>
 <body>
-<x-header/>
+<x-header />
+@if(request()->is('/'))
+    <x-hero />
+@endif
 <main class="container mx-auto p-4 mt-4 bg-amber-100">
     {{$slot}}
 </main>
-<script src="{{asset('js/script.js')}}"></script>
+{{--<script src="{{asset('js/script.js')}}"></script>--}}
 </body>
 </html>
