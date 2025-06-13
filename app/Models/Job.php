e9281d7bc28caa76bed5d0b3d4a8a6be61404e44
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-14 00:01:38 +0100

    feat(job-schema): create migration to add fields to job listings table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,11 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 
 class Job extends Model
 {
    protected $table = 'job_listings';
 
-   protected $fillable = ['title', 'description'];
+   protected $fillable = [
+       'title',
+       'description',
+       'salary',
+       'tags',
+       'job_type',
+       'remote',
+       'requirements',
+       'benefits',
+       'address',
+       'city',
+       'state',
+       'zipcode',
+       'contact_email',
+       'contact_phone',
+       'company_name',
+       'company_description',
+       'company_logo',
+       'company_website',
+       'user_id'
+   ];
+
+   // Relation to user
+   public function user(): BelongsTo
+   {
+       return $this->belongsTo(User::class);
+   }
 }
