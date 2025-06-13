080e39334ee16b227dbeeca1c1cc26728a283164
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-13 14:49:10 +0100

    fix(mobile-men): fix nav links components for mobile

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -4,8 +4,9 @@
     'bgClass' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
     'textClass' => 'text-black',
+    'block' => false,
 ])
-<a href="{{$url}}" class="{{$bgClass}} {{$hoverClass}} {{$textClass}} px-4 py-2 rounded hover:shadow-md transition duration-300">
+<a href="{{$url}}" class="{{$bgClass}} {{$hoverClass}} {{$textClass}} {{$block ? 'block' : ''}} px-4 py-2 rounded hover:shadow-md transition duration-300">
     @if($icon)
         <i class="fa fa-{{$icon}} mr-1"></i>
     @endif
